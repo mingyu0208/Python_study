7a97ad6699c1dd50d3401bf536dcfdbec855b8a1
mingyu0208 <mingyu99809820@gmail.com>
2024-08-19 23:00:08 +0900

    11:00

--- 함수_3.py
+++ 함수_3.py
@@ -35,3 +35,94 @@
  # 결론: 가변 매개변수가 우선된다.
 # ==========================================
 
+# 이런식으로 가능함.
+
+# def test(a, b = 10, c = 100):
+#     print(a+b+c)
+
+# test(10, 20, 30)
+
+# test(a = 10, b = 100, c = 200)
+
+# test(c=10,a=100,b=200)
+
+# test(10,c=200)
+# ==========================================
+
+# 팩토리얼 함수
+# n! = n * (n - 1) * (n - 2) * ... * 1
+# def factorial(n):
+#     output = 1
+
+#     for i in range(1,n+1):
+#         output *=i
+#     return output
+
+# print("1!:", factorial(1))
+# print("2!:", factorial(2))
+# print("3!:", factorial(3))
+# print("4!:", factorial(4))
+# print("5!:", factorial(5))
+
+# ==========================================
+
+# 재귀함수로 팩토리얼 구하기
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+
+# print("1!:", factorial(1))
+# print("2!:", factorial(2))
+# print("3!:", factorial(3))
+# print("4!:", factorial(4))
+# print("5!:", factorial(5))
+# ==========================================
+
+# 재귀 함수로 구현한 피보나치 수열(1)
+# def fibonacci(n):
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 1
+#     else:
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+
+# print("1!:", fibonacci(1))
+# print("2!:", fibonacci(2))
+# print("3!:", fibonacci(3))
+# print("4!:", fibonacci(4))
+# print("5!:", fibonacci(5))
+
+# ==========================================
+# counter = 0
+# def fibonacci(n):
+#     global counter    #global 외부 참조
+#     counter += 1
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 1
+#     else:
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+# print(fibonacci(10))
+
+# dictionary = {
+#     1:1,
+#     2:1
+# }
+
+# def fibonacci(n):
+#     if n in dictionary:
+#         return dictionary[n]
+#     else:
+#         output = fibonacci(n - 1) + fibonacci(n - 2)
+#         dictionary[n] = output
+#         return output
+
+# print("10!:", fibonacci(10))
+# print("20!:", fibonacci(20))
+# print("30!:", fibonacci(30))
+# print("40!:", fibonacci(40))
+# print("50!:", fibonacci(50))
